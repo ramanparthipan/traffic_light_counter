f291ad38f1b2f8237f495ebaf7beda9e83e4b316
R. Parthipan <rp732@cam.ac.uk>
2025-02-25 14:09:11 +0000

    Completion of task 1

--- ftr_tlc.vhd
+++ ftr_tlc.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity ftr_tlc is
 	port(
@@ -16,7 +17,8 @@ architecture tlc_arch of ftr_tlc is
 	type state_type is (G, Y, R, W);
 	signal state: state_type;
 	signal tick: std_logic;
-	signal bcd: std_logic_vector(3 downto 0);
+	signal bcd0: std_logic_vector(3 downto 0);
+	signal bcd1: std_logic_vector(3 downto 0);
 	signal en: std_logic;
 	component counter
 		port(clk: in std_logic;
@@ -32,39 +34,46 @@ begin
 		port map(clk=>clk, slow_clk=>tick);
 		
 		process (tick, reset)
-			variable count : INTEGER;
+			variable timer : INTEGER;
 		begin
 			if reset = '0' then
 				state <= G;
-			elsif rising_edge(tick) then -- using this means the request button needs to be pushed for ~1s for it to be registered.
+			elsif rising_edge(tick) then -- request button needs to be pushed for ~1s for it to be registered.
 				case state is
 					when G =>
 						if request = '0' then
 							state <= Y;
-							count := 0;
+							timer := 5;
 						end if;
 					when Y =>
-						if count = 5 then
+						if timer = 0 then
 							state <= R;
-							count := 0;
+							timer := 10;
 						else
-							count := count + 1;
+							timer := timer - 1;
 						end if;
 					when R => 
-						if count = 10 then 
+						if timer = 0 then 
 							state <= W;
-							count := 0;
+							timer := 10;
 						else
-							count := count + 1;
+							timer := timer - 1;
 						end if;
 					when W => -- wait state to prevent traffic clogging
-					 if count = 10 then 
+					 if timer = 0 then 
 							state <= G;
-							count := 0;
+							timer := 0; -- doesn't matter
 						else
-							count := count + 1;
+							timer := timer - 1;
 						end if;
 				end case;
+			end if;
+			if timer = 10 then
+				bcd0 <= std_logic_vector(to_unsigned(0, 4));
+				bcd1 <= std_logic_vector(to_unsigned(1, 4));
+			else
+				bcd0 <= std_logic_vector(to_unsigned(timer, 4));
+				bcd1 <= std_logic_vector(to_unsigned(0, 4));
 			end if;
 		end process;
 	
@@ -73,22 +82,23 @@ begin
 		case state is
 			when G => 
 				output <= "10001";
+				en <= '0';
 			when Y =>
 				output <= "10010";
+				en <= '1';
 			when R =>
 				output <= "01100";
+				en <= '1';
 			when W =>
 				output <= "10001"; --same as green
+				en <= '0';
 		end case;
 	end process;
 	
-	en <= '1';
-	bcd <= "0110";
-	
 	u2: bcd7seg
-	port map(bcd=>bcd, en=>en, hex=>hex0);
+	port map(bcd=>bcd0, en=>en, hex=>hex0);
 	u3: bcd7seg
-	port map(bcd=>bcd, en=>en, hex=>hex1);
+	port map(bcd=>bcd1, en=>en, hex=>hex1);
 	
 	
 	
